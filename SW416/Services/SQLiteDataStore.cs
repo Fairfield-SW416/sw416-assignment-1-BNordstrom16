20854109d7398447e0464747e7a1302c8c5d1dd7
BNordstrom16 <bradley.nordstrom@student.fairfield.edu>
2018-01-31 20:03:48 -0500

    Commented out the SQL data store

--- SW416/Services/SQLiteDataStore.cs
+++ SW416/Services/SQLiteDataStore.cs
@@ -1,40 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Threading.Tasks;
 
-using Newtonsoft.Json;
-using Plugin.Connectivity;
+//using Newtonsoft.Json;
+//using Plugin.Connectivity;
 
-namespace SW416
-{
-    public class SQLiteDataStore : IDataStore<Item>
-    {
-        IEnumerable<Item> items;
+//namespace SW416
+//{
+//    public class SQLiteDataStore : IDataStore<Item>
+//    {
+//        IEnumerable<Item> items;
 
-        public SQLiteDataStore()
-        {
-            items = new List<Item>();
-        }
+//        public SQLiteDataStore()
+//        {
+//            items = new List<Item>();
+//        }
 
-        public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
-        {
-        }
+//        public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
+//        {
+//        }
 
-        public async Task<Item> GetItemAsync(string id)
-        {
-        }
+//        public async Task<Item> GetItemAsync(string id)
+//        {
+//        }
 
-        public async Task<bool> AddItemAsync(Item item)
-        {
-        }
+//        public async Task<bool> AddItemAsync(Item item)
+//        {
+//        }
 
-        public async Task<bool> UpdateItemAsync(Item item)
-        {
-        }
+//        public async Task<bool> UpdateItemAsync(Item item)
+//        {
+//        }
 
-        public async Task<bool> DeleteItemAsync(string id)
-        {
-        }
-    }
-}
+//        public async Task<bool> DeleteItemAsync(string id)
+//        {
+//        }
+//    }
+//}
